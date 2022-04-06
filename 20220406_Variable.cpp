ed2909f59cd7bd38a549ab479be0caede868f05e
JustinGantengNO1 <103086069+JustinGantengNO1@users.noreply.github.com>
2022-04-06 11:21:11 +0800

    Create 20220406_Variable.cpp

---
+++ 20220406_Variable.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+    float angka;
+    string nama; //deklarasi variabel
+    nama = "Justin Ray Harrison"; //masukkan value kedalam variabel
+    angka = 10.21*3.14;
+    cout<<nama<<endl; //menanmpilkan variabel
+    cout<<angka;
+
+}//end of function
